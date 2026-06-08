14819b154221f8dcd3206e0a5dbfdbaa68a7b391
evan2110 <duc21102001@gmail.com>
2026-06-09 06:37:52 +0700

    Test release version

--- NhatDucSoftware/LoginForm.cs
+++ NhatDucSoftware/LoginForm.cs
@@ -44,7 +44,7 @@ public class LoginForm : Form
         var lblCopyright = new Label
         {
             AutoSize = true,
-            Text = "Make by Nhật Đức",
+            Text = "Make by Nhật Duck",
             Left = 10,
             Top = ClientSize.Height - 22,
             Anchor = AnchorStyles.Left | AnchorStyles.Bottom
